793de7154f71605598ee5219772be9d62786e2ee
balumbungan <desabalumbungan@gmail.com>
2023-09-10 22:15:50 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# WARSPEAR AMPLIFICATION SIMULATION
+
+Adalah sebuat website untuk melakukan simulasi Amplification (Ampli Weapon Agar Menambah Damage)
+
+Website ini adalah website FAN MADE, dimana saya sebagai pembuat tools tidak bermaksud menduplikat fitur apapun di dalam game tersebut.
+
+Algoritma Ampli di web ini adalah menggunakan rumus random, di setiap up level, maka nilai random akan dipangkatkan sehingga semakin banyak Plusnya (+) maka semakin sulit kesempatan amplinya.
+
+## Link Web / Simulation
+
+https://wahyuhidayattz.github.io/warspear-amply-simulation
+
+## Teknologi yang digunakan
+
+- Javascript (Sebagai mesin algoritma amply)
+- Tailwindcss (Sebagai tampilai UI)
+- HTML (Sebagai struktur dasar WEB)
+
+## Credit
+
+- Warspear Online (Official Site) untuk resources gambar dan background
+
+- Website : https://warspear-online.com/en/
+
+PLAY WARSPEAR EVERYDAY
---
+++ items.js
@@ -0,0 +1,32 @@
+let items = [
+    {
+        'name': 'Blade of Kingdom of Fortitude',
+        'icon': 'res/1-handed-weapon.png',
+        'type': 'one-handed-weapon'
+    },
+    {
+        'name': 'Poleaxe of Kingdom of Fortitude',
+        'icon': 'res/2-handed-weapon.png',
+        'type': 'two-handed-weapon'
+    },
+    {
+        'name': 'Crossbow of Kingdom of Fortitude',
+        'icon': 'res/ranged-weapon-crossbow.png',
+        'type': 'ranged-weapon'
+    },
+    {
+        'name': 'Bow of Kingdom of Fortitude',
+        'icon': 'res/ranged-weapon-bow.png',
+        'type': 'ranged-weapon'
+    },
+    {
+        'name': 'Staff of Kingdom of Fortitude',
+        'icon': 'res/ranged-weapon-staff.png',
+        'type': 'ranged-weapon'
+    },
+    {
+        'name': 'Shiled of Kingdom of Fortitude',
+        'icon': 'res/shield.png',
+        'type': 'one-handed-weapon'
+    },
+]
