409cef1ae3b7cd99ba52617e304824f7b40cd3fc
balumbungan <desabalumbungan@gmail.com>
2023-09-15 22:15:59 +0700

    Update List Item dan Add Toast

--- items.js
+++ items.js
@@ -1,32 +1,77 @@
 let items = [
     {
         'name': 'Blade of Kingdom of Fortitude',
-        'icon': 'res/1-handed-weapon.png',
+        'icon': 'res/blade-of-kingdom-of-fortitude.png',
         'type': 'one-handed-weapon'
     },
     {
-        'name': 'Poleaxe of Kingdom of Fortitude',
-        'icon': 'res/2-handed-weapon.png',
+        'name': 'Blade of Metamorphosis',
+        'icon': 'res/blade-of-metamorphosis.png',
+        'type': 'one-handed-weapon'
+    },
+    {
+        'name': 'Dagger of Kingdom of Fortitude',
+        'icon': 'res/dagger-of-kingdom-of-fortitude.png',
+        'type': 'one-handed-weapon'
+    },
+    {
+        'name': 'Crossbow of Kingdom of Indestructibility',
+        'icon': 'res/corssbow-of-kingdom-of-indestructibility.png',
+        'type': 'ranged-weapon'
+    },
+    {
+        'name': 'Crossbow of Metamorphosis',
+        'icon': 'res/corssbow-of-metamorphosis.png',
+        'type': 'ranged-weapon'
+    },
+    {
+        'name': 'Hammer of Kingdom of Fortitude',
+        'icon': 'res/hammer-of-kingdom-of-fortitude.png',
         'type': 'two-handed-weapon'
     },
     {
-        'name': 'Crossbow of Kingdom of Fortitude',
-        'icon': 'res/ranged-weapon-crossbow.png',
+        'name': 'Royal Axe of the Magic Bastion',
+        'icon': 'res/royal-axe-of-the-magic-bastion.png',
+        'type': 'one-handed-weapon'
+    },
+    {
+        'name': 'Royal Blade of the Magic Bastion',
+        'icon': 'res/royal-blade-of-the-magic-bastion.png',
+        'type': 'one-handed-weapon'
+    },
+    {
+        'name': 'Royal Bow of the Magic Bastion',
+        'icon': 'res/royal-bow-of-the-magic-bastion.png',
         'type': 'ranged-weapon'
     },
     {
-        'name': 'Bow of Kingdom of Fortitude',
-        'icon': 'res/ranged-weapon-bow.png',
-        'type': 'ranged-weapon'
+        'name': 'Royal Dagger of the Magic Bastion',
+        'icon': 'res/royal-dagger-of-the-magic-bastion.png',
+        'type': 'one-handed-weapon'
     },
     {
-        'name': 'Staff of Kingdom of Fortitude',
-        'icon': 'res/ranged-weapon-staff.png',
-        'type': 'ranged-weapon'
+        'name': 'Royal Hammer of the Magic Bastion',
+        'icon': 'res/royal-hammer-of-the-magic-bastion.png',
+        'type': 'two-handed-weapon'
     },
     {
-        'name': 'Shiled of Kingdom of Fortitude',
-        'icon': 'res/shield.png',
+        'name': 'Royal Mace of the Magic Bastion',
+        'icon': 'res/royal-mace-of-the-magic-bastion.png',
+        'type': 'one-handed-weapon'
+    },
+    {
+        'name': 'Royal Sword of the Magic Bastion',
+        'icon': 'res/royal-sword-of-the-magic-bastion.png',
+        'type': 'one-handed-weapon'
+    },
+    {
+        'name': 'Royal Shield of the Magic Bastion',
+        'icon': 'res/royal-shield-of-the-magic-bastion.png',
         'type': 'shield'
+    },
+    {
+        'name': 'Scepter of Kingdom of Fortitude',
+        'icon': 'res/scepter-of-kingdom-of-fortitude.png',
+        'type': 'ranged-weapon'
     },
 ]
